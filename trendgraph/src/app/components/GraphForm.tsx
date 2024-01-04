a9980c83c1072bd4e489955b9343d5910420bf82
Exerea <ryu.vs.lime.and.takumi@ezweb.ne.jp>
2024-01-05 01:53:20 +0900

    Feat:選択フォームの選択状況を反映するように

--- trendgraph/src/app/components/GraphForm.tsx
+++ trendgraph/src/app/components/GraphForm.tsx
@@ -1,16 +1,17 @@
-'use client'
-import React, { useCallback, useEffect, useMemo, useState } from 'react'
-import Highcharts, { SeriesOptionsType } from "highcharts"
-import HighchartsReact from 'highcharts-react-official';
-import { requestInit } from '@/api/fetcher';
-
+"use client";
+import React, { useCallback, useEffect, useMemo, useState } from "react";
+import Highcharts, { SeriesOptionsType } from "highcharts";
+import HighchartsReact from "highcharts-react-official";
+import { requestInit } from "@/api/fetcher";
+import { prefCodesState } from "@/recoil/atoms/checkstate";
+import { useRecoilValue } from "recoil";
 
 /**
  * Graphフォーム
  * {@link SelectionForm}のチェックボックスのtoggle状況で再レンダーする
  * @returns 構築済みフォーム
-*/
-function option(val: SeriesOptionsType[]) {
+ */
+function option(val: SeriesOptionsType[], categories: number[]) {
   return {
     title: {
       text: "",
@@ -24,51 +25,50 @@ function option(val: SeriesOptionsType[]) {
       title: {
         text: "年度",
       },
-      categories: [],
+      categories: categories,
     },
     series: val,
-  }
+  };
 }
 
 const Graph: React.FC = () => {
-
-
-
-
   const name: string = `this is ${1}`;
 
+  const preCodes: number[] = useRecoilValue(prefCodesState);
 
-  // const preCodes: number[] = [1, 2];// useRecoilValue(prefCodesState);
-  const preCodes = useMemo(() => [3, 4], []);
-
-
-  const [seriesOptionsType, setSeriesOptionsType] = useState<SeriesOptionsType[]>([]);
+  const [seriesOptionsType, setSeriesOptionsType] = useState<
+    SeriesOptionsType[]
+  >([]);
+  const [categories, setCategories] = useState<number[]>([]);
 
   type record = {
-    year: number,
-    value: string
+    year: number;
+    value: string;
   };
 
   /**
-     * SWR arrayFetcher構築
-     * @param urlArr 
-     * @returns 
-     */
+   * SWR arrayFetcher構築
+   * @param urlArr
+   * @returns
+   */
   const arrayFetcher = useCallback(async (preCodes: number[]) => {
-
+    console.log("error fetching");
     //都道府県コードからURLコード群に変換
     //NOTE:RESASは1クエリ条件1precodeのため
-    const urls: string[] = preCodes.map(code => {
+    const urls: string[] = preCodes.map((code) => {
       const URL = "https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear";
       return `${URL}?prefCode=${code}`;
     });
 
     //レスポンスからデータ抽出
     try {
-
       //取得データ
-      const responses = await Promise.all(urls.map(url => fetch(url, requestInit)));
-      const data = await Promise.all(responses.map(response => response.json()));
+      const responses = await Promise.all(
+        urls.map((url) => fetch(url, requestInit))
+      );
+      const data = await Promise.all(
+        responses.map((response) => response.json())
+      );
 
       //オプション変換
       const series: SeriesOptionsType[] = [];
@@ -78,35 +78,39 @@ const Graph: React.FC = () => {
         const val: SeriesOptionsType = {
           name: name,
           data: values,
-          type: 'line'
+          type: "line",
         };
         series.push(val);
       }
       setSeriesOptionsType(series);
 
+      const record: record[] = data[0].result.data[0].data;
+      const years: number[] = record.map((val: record) => val.year);
+      setCategories(years);
     } catch (error) {
       console.log("error fetching");
     }
-
-  }, []);
-
-
+  }, [preCodes]);
 
   //preCodesが変更されるたび再構築
   useEffect(() => {
     arrayFetcher(preCodes);
-  }, []);
+  }, [preCodes]);
 
-
-
-  return <HighchartsReact key={"resas_chart"} highcharts={Highcharts} options={option(seriesOptionsType)} />;
-}
+  return (
+    <HighchartsReact
+      key={"resas_chart"}
+      highcharts={Highcharts}
+      options={option(seriesOptionsType, categories)}
+    />
+  );
+};
 
 const GraphForm: React.FC = () => {
   return (
     <div>
       <Graph></Graph>
-    </div >
+    </div>
   );
 };
 
