9106587ddcf92800d3417f9e4d53157ac3dc9366
Exerea <ryu.vs.lime.and.takumi@ezweb.ne.jp>
2024-01-13 13:22:41 +0900

    Chore:古いJSDocを更新

--- trendgraph/src/app/components/SelectionForm.tsx
+++ trendgraph/src/app/components/SelectionForm.tsx
@@ -3,45 +3,48 @@ import useSWR from "swr";
 import React from "react";
 import CheckBox from "./CheckBox";
 
+/**
+ * チェックボックスフォームを作成します。
+ * - 表示データが取得できない場合は、別途メッセージを表示します。
+ * - 取得できた場合は各都道府県の{@link @/CheckBox}を表示します。
+ * @returns {React.ReactElement} チェックボックスフォーム
+ */
 const CheckBoxes: React.FC = () => {
-  const URL: string = "https://opendata.resas-portal.go.jp/api/v1/prefectures";
-  const { data, error } = useSWR(URL, fetcher);
+    const URL: string = "https://opendata.resas-portal.go.jp/api/v1/prefectures";
+    const { data, error, isLoading } = useSWR(URL, fetcher);
 
-  //リクエスト失敗
-  if (error) {
-    return <div>リクエスト失敗</div>;
-  }
-
-  //リクエスト成功
-  else {
-    if (data) {
-      //CheckBoxを作成していく
-      const prefactures: Prefacture[] | [] = data.result;
-      return (
-        <>
-          {prefactures.map((prefacture: Prefacture) => (
-            <CheckBox key={prefacture.prefCode} prefacture={prefacture} />
-          ))}
-        </>
-      );
+    //リクエスト失敗
+    if (error) {
+        return <div>リクエスト失敗</div>;
     }
 
-    return <div>データ読み込み中</div>;
-  }
+    //ローディング中
+    if (isLoading) {
+        return <div>データ読み込み中</div>;
+    }
+
+    //チェックボックス群
+    const prefactures: Prefacture[] | [] = data.result;
+    return (
+        <>
+            {prefactures.map((prefacture: Prefacture) => (
+                <CheckBox key={prefacture.prefCode} prefacture={prefacture} />
+            ))}
+        </>
+    );
 };
 
 /**
- * 選択フォーム
- * @param prefParams
- * @returns
+ * 選択フォームを構築します。
+ * @returns {React.ReactElement} 選択フォーム
  */
 const SelectionForm: React.FC = () => {
-  return (
-    <div>
-      <h3>都道府県</h3>
-      <CheckBoxes></CheckBoxes>
-    </div>
-  );
+    return (
+        <div>
+            <h3>都道府県</h3>
+            <CheckBoxes></CheckBoxes>
+        </div>
+    );
 };
 
 export default SelectionForm;
